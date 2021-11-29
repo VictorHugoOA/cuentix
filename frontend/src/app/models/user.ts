fbdd9ed8c94a6e4737b26731b072910262de0b2a
Jess Guerrero <57547113+itsJessGC@users.noreply.github.com>
2021-11-29 15:19:59 -0600

    models

---
+++ frontend/src/app/models/user.ts
@@ -0,0 +1,9 @@
+export class User {
+    constructor(
+        public usuario: string,
+        public email: string,
+        public password: string,
+        public passwordCon: string,
+        public type: string
+    ){}
+}

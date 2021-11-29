e920429386c34aea9cc2b81f0b3e9917bba05237
LadySanchez <57550682+LadySanchez@users.noreply.github.com>
2021-11-29 14:39:56 -0600

    Add files via upload

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
@@ -10,6 +12,8 @@ import { ListProductsComponent } from './components/list-products/list-products.
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { ProductComponent } from './components/product/product.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { BuyedAccountsComponent } from './components/buyed-accounts/buyed-accounts.component';
+import { BuyAccountDetailsComponent } from './components/buy-account-details/buy-account-details.component';
 
 @NgModule({
   declarations: [
@@ -20,11 +24,16 @@ import { ProfileComponent } from './components/profile/profile.component';
     ListProductsComponent,
     NavBarComponent,
     ProductComponent,
-    ProfileComponent
+    ProfileComponent,
+    BuyedAccountsComponent,
+    BuyAccountDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule    
   ],
   providers: [],
   bootstrap: [AppComponent]

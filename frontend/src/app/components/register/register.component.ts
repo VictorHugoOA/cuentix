06455793a9e8a21913f0602a8b0c4c95bda8cbfb
VictorHugoOA <victoralvarez.vhoa@gmail.com>
2021-11-30 00:38:35 -0600

    Details

--- frontend/src/app/components/register/register.component.ts
+++ frontend/src/app/components/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { User } from 'src/app/models/user';
+import { User } from './../../models/user';
 import { RegistroService } from 'src/app/services/registro/registro.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -9,12 +10,20 @@ import { RegistroService } from 'src/app/services/registro/registro.service';
 })
 export class RegisterComponent implements OnInit {
 
-  userModel = new User('', '', '', '','Comprador');
-  constructor(private usuario: RegistroService) { }
+  userModel = new User('', '', '', '','Selecciona tipo de usuario');
+  public userForm: FormGroup;
+  constructor(private usuario: RegistroService, private formBuilder: FormBuilder) {
+    this.userForm = this.formBuilder.group({
+      usuario: ['', Validators.required, Validators.minLength(6), Validators.maxLength(255)],
+      email: ['', Validators.required, Validators.email],
+      tipo: ['' ,Validators.required],
+      
+    })
+  }
 
   nuevoUsr(){
     console.log(this.userModel)
-    this.usuario.newUser(this.userModel);
+    //this.usuario.newUser(this.userModel);
   }
 
   ngOnInit(): void {

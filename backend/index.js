c6e66cc25be63683845e1c053360dea83e38d44d
VictorHugoOA <victoralvarez.vhoa@gmail.com>
2021-12-02 13:43:01 -0600

    Cambio del puerto

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
-const port = 3004;
+const port = 3000;
 require("dotenv").config();
 //const io = require("socket.io")(3001);
 
